00448b022fb58072bbb172fd3d851592a7962b2b
Jimmy Millard <millardjimmy@gmail.com>
2020-05-21 04:17:32 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -14,6 +14,12 @@ class Waiter
     @@all
   end
   
+  def meals
+    Meal.all.select do |meal|
+      meal.waiter == self 
+    end
+  end 
+  
   def new_meal(customer, total, tip=0)
       meal.new(self, customer, total, tip)
     end 
